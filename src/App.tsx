c157aa34d2cbfc828e03a1c5a8c070ad56f5deb2
Amr Taha <amrshiha2@gmail.com>
2025-04-15 23:17:24 +0200

    Add basic routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Routes, Route } from "react-router";
+import AuthLayout from "./Providers/AuthLayout";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Customers from "./pages/Customers";
+import Employees from "./pages/Employees";
+import Privacy from "./pages/Privacy";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <div className="text-center">Hello World!</div>;
+  return (
+    <Routes>
+      {/* Public Route */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Protected Route */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/privacy" element={<Privacy />} />
+      </Route>
+
+      {/* Fallback */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
