977d81f52bbff2899415591e370ee2b111fa4b2c
Fatma799 <fatmamagdy20055@gmail.com>
2025-04-16 22:53:38 +0200

    fatma
    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
+import './styles/main.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
