eed8ff670d63987cbe4730648f1ae84aa5311bdc
Amr Taha <amrshiha2@gmail.com>
2025-04-26 01:19:52 +0300

    refactor layout and sidebar structure

--- src/components/Layout/DashboardLayout/DashboardLayout.tsx
+++ src/components/Layout/DashboardLayout/DashboardLayout.tsx
@@ -3,48 +3,33 @@ import { Outlet } from "react-router-dom";
 import RightSidebar from "../Sidebar/RightSidebar/RightSidebar";
 // import LeftSidebar from "../Sidebar/Left Sidebar/LeftSidebar";
 import Header from "../Header/Header";
-import { Menu } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 interface DashboardLayoutProps {
   children?: React.ReactNode;
 }
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
   return (
-    <div className="dashboard-layout flex ">
-      {/* <LeftSidebar /> */}
+    <SidebarProvider>
+      <Sidebar side="left">
+        <RightSidebar />
+      </Sidebar>
 
-      <div className="flex-1 flex flex-col">
-        <button className="md:hidden p-4" onClick={toggleSidebar}>
-          <Menu />
-        </button>
+      <div className="grow flex flex-col">
+        <SidebarTrigger className="md:hidden" />
         <Header />
 
-        <div className="main-content">
+        <div className="main-content px-4 md:px-10">
           <Outlet />
         </div>
       </div>
-
-      {/* Sidebar for desktop */}
-      <div className="hidden md:block w-64">
-        <RightSidebar isOpen={true} onClose={toggleSidebar} />
-      </div>
-
-      {/* Sidebar for mobile */}
-      <div className="md:hidden">
-        {isSidebarOpen && (
-          <div className="absolute z-50 w-64 h-full shadow-lg">
-            <RightSidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
-          </div>
-        )}
-      </div>
-    </div>
+    </SidebarProvider>
   );
 };
 
--- src/components/Layout/Sidebar/RightSidebar/RightSidebar.tsx
+++ src/components/Layout/Sidebar/RightSidebar/RightSidebar.tsx
@@ -1,14 +1,9 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
 import { Home, Users, User, ShoppingCart, Coins, Settings } from "lucide-react";
-import logo from "../../../../assets/logo/logolight.svg";
+import logo from "@/assets/logo/logolight.svg";
+import { Link } from "react-router";
+import { useLocation } from "react-router";
 
-interface RightSidebarProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
-
-const RightSidebar: React.FC<RightSidebarProps> = ({ isOpen, onClose }) => {
+const RightSidebar = () => {
   const location = useLocation();
 
   const menuItems = [
@@ -19,52 +14,50 @@ const RightSidebar: React.FC<RightSidebarProps> = ({ isOpen, onClose }) => {
     { path: "/costs", icon: <Coins size={27} />, text: "التكاليف" },
     { path: "/privacy", icon: <Settings size={27} />, text: "الخصوصية" },
   ];
-
   return (
-    <>
-      <aside className={`rightSidebar ${isOpen ? "open" : "closed"}`}>
-        <button
-          className="toggle-sidebar md:!hidden"
-          onClick={onClose}
-          aria-label="Toggle sidebar"
-        >
-          {isOpen ? "✕" : "☰"}
-        </button>
-
-        <div className="sidebar-header">
+    <aside className="bg-primary-color h-full w-full">
+      <div className="flex flex-col py-8">
+        <div className="flex justify-center">
           <img src={logo} alt="Enjaz" className="logo" />
         </div>
 
-        <nav className="pro-menu">
-          <ul>
-            {menuItems.map((item) => (
-              <li
-                key={item.path}
-                className={`pro-menu-item ${
-                  location.pathname === item.path ? "active" : ""
-                }`}
-              >
-                <Link to={item.path} className="menu-link">
-                  <span className="pro-icon-wrapper">{item.icon}</span>
-                  <span className="pro-item-content">{item.text}</span>
-                </Link>
-              </li>
-            ))}
+        <nav className="mt-24">
+          <ul className="px-3.5 md:pl-3.5 md:pr-0 rtl:md:pr-3.5 rtl:md:pl-0 font-medium text-xl text-white">
+            {menuItems.map((item) => {
+              const isActive = location.pathname === item.path;
+              return (
+                <li
+                  key={item.path}
+                  className={`relative hover:bg-white md:hover:bg-transparent hover:text-primary-color md:hover:text-white hover:bg-gradient-to-r rtl:hover:bg-gradient-to-l md:from-white md:to-[75%] md:to-transparent hover:rounded-full md:hover:rounded-l-full md:hover:rounded-r-none rtl:md:hover:rounded-r-full rtl:md:hover:rounded-l-none ${
+                    isActive
+                      ? "md:hover:bg-white md:hover:text-primary-color bg-white text-primary-color rounded-full md:rounded-l-full text-[1.375rem] font-bold"
+                      : ""
+                  }`}
+                >
+                  <Link
+                    to={item.path}
+                    className={`flex items-center rtl:justify-start rtl:flex-row gap-x-4 px-8 md:pl-8 md:pr-0 rtl:md:pr-8 rtl:md:pl-0 ${
+                      isActive ? "py-5" : "py-2.5"
+                    }`}
+                  >
+                    <span>{item.icon}</span>
+                    <span>{item.text}</span>
+                  </Link>
+
+                  {isActive && (
+                    <>
+                      <div className="hidden md:block absolute right-0 rtl:left-0 rtl:right-auto w-1/5 -inset-y-10 bg-white" />
+                      <div className="hidden md:block absolute bottom-full right-0 w-1/5 rtl:left-0 rtl:right-auto -inset-y-10 bg-primary-color rounded-br-full rtl:rounded-bl-full rtl:rounded-br-none" />
+                      <div className="hidden md:block absolute top-full right-0 w-1/5 rtl:left-0 rtl:right-auto -inset-y-10 bg-primary-color rounded-tr-full rtl:rounded-tl-full rtl:rounded-tr-none" />
+                    </>
+                  )}
+                </li>
+              );
+            })}
           </ul>
         </nav>
-        {/* <main className="main-content">{children}</main> */}
-      </aside>
-
-      {!isOpen && (
-        <button
-          className="toggle-sidebar outside"
-          onClick={onClose}
-          aria-label="Open sidebar"
-        >
-          ☰
-        </button>
-      )}
-    </>
+      </div>
+    </aside>
   );
 };
 
